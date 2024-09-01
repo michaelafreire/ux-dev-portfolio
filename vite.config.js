33a1b30c8dfa2faf1e70dd47b5dd3276c04249d0
Michaela Freire <michaela.freiregriffith@gmail.com>
2024-09-01 20:40:55 +0100

    continue tryign:

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/ux-dev-portfolio/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx'], // Ensure Vite resolves .jsx files
   },
-})
+});
