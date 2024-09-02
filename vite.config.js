9e47d80b2cc5269c195d1c6731d9abb3065a4cd8
Michaela Freire <michaela.freiregriffith@gmail.com>
2024-09-02 10:40:48 +0100

    build

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ux-dev-portfolio/',
+  base: '/ux-dev-portfolio/', // Ensure this matches your GitHub repository name
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.jsx'], // Ensure Vite resolves .jsx files
+    alias: {
+      '@': '/src', // Add alias for src directory
+    },
+    extensions: ['.js', '.jsx'],
   },
 });
